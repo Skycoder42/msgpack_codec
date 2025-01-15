22adeeee7cbdfefd4a0b9ed858c016b367209e1a
Skycoder42 <Skycoder42@users.noreply.github.com>
2025-01-15 07:43:09 +0000

    fix pana score

---
+++ example/example.dart
@@ -0,0 +1,28 @@
+// ignore_for_file: avoid_print
+
+import 'dart:typed_data';
+
+import 'package:msgpack_dart/msgpack_dart.dart';
+
+void main(List<String> args) {
+  final data = {
+    'name': 'John Doe',
+    'age': 30,
+    'isEmployed': true,
+    'children': [
+      {'name': 'Alice', 'age': 7},
+      {'name': 'Bob', 'age': 10},
+    ],
+  };
+
+  final encoded = msgPack.encode(data);
+
+  print('Encoded: ${_toHexString(encoded)}');
+
+  final decoded = msgPack.decode(encoded);
+
+  print('Decoded: $decoded');
+}
+
+String _toHexString(Uint8List bytes) =>
+    bytes.map((i) => i.toRadixString(16).padLeft(2, '0')).join('-');
